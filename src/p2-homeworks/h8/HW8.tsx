1ccf806905c96fac8ac1b06b60c5b2f373089141
Serjge <serjge@yandex.ru>
2022-01-07 13:36:37 +0300

    Creat alternative 8 homework

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
+import {homeWorkReducer, sortFilter} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
 
 export type UserType = {
     _id: number,
@@ -28,9 +29,39 @@ function HW8() {
         </div>
     ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortUp = () => {
+        setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+        console.log('sortUp')
+    }
     const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
     const sortCheck = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
+
+    const AlternativeInitialPeople = [
+        {_id: 0, name: 'Кот', age: 3},
+        {_id: 1, name: 'Александр', age: 66},
+        {_id: 2, name: 'Коля', age: 16},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 4, name: 'Ярослав', age: 40},
+        {_id: 5, name: 'Ирина', age: 55},
+        {_id: 6, name: 'Дмитрий', age: 45},
+    ]
+
+
+    const arrCheck = [0, 18, 65]
+    const arrFilter = ['default','up', 'down']
+
+    const [value, setValue] = useState(arrCheck[0])
+    const [valueFilter, setValueFilter] = useState<sortFilter>('default')
+
+    const filterPeople = homeWorkReducer(AlternativeInitialPeople, {type: 'check-up', payload: {age: value, filter: valueFilter}})
+
+
+    const AlternativePeople = filterPeople.map((p: UserType) => (
+        <div key={p._id} style={{display: 'flex', justifyContent: 'space-between', width: '300px'}}>
+            <span> {p.name}</span>
+            <span>{p.age}</span>
+        </div>
+    ))
 
     return (
         <div>
@@ -47,7 +78,14 @@ function HW8() {
 
 
             <hr/>
-
+            {AlternativePeople}
+            Filter age: <SuperSelect options={arrCheck}
+                         onChangeOption={setValue}
+                         value={value}
+            />   Sort Name: <SuperSelect options={arrFilter}
+                         onChangeOption={setValueFilter}
+                         value={valueFilter}
+            />
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
             <hr/>
@@ -56,5 +94,4 @@ function HW8() {
 }
 
 export default HW8
-
 
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -13,15 +13,41 @@ export const homeWorkReducer = (state: UserType[], action: actionType): UserType
         case 'check': {
             return state.filter(a => a.age >= action.payload)
         }
-               default:
+        case 'check-up': {
+            const sortArrName = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1;
+                if (a.name < b.name) return -1;
+                return 0;
+            })
+            // обычный ситаксис
+            // let result: UserType[]
+            //
+            // if (action.payload.filter === 'up') {
+            //    result = sortArrName.filter(a => a.age >= action.payload.age)
+            // }else if (action.payload.filter === 'down') {
+            //     result = sortArrName.reverse().filter(a => a.age >= action.payload.age)
+            // }else {
+            //     result = state.filter(a => a.age >= action.payload.age)
+            // }
+            // return result
+
+            //синтаксис тернарником
+            return (action.payload.filter === 'up')
+                ? sortArrName.filter(a => a.age >= action.payload.age)
+                : (action.payload.filter === 'down')
+                    ? sortArrName.reverse().filter(a => a.age >= action.payload.age)
+                    : state.filter(a => a.age >= action.payload.age)
+        }
+
+        default:
             return state
     }
 }
 
-export type sortFilter = 'up' | 'down'
+export type sortFilter = 'default' | 'up' | 'down'
 type actionType = ReturnType<typeof sortAC>
     | ReturnType<typeof checkAC>
-
+    | ReturnType<typeof checkUpAC>
 
 const sortAC = (filter: sortFilter) => {
     return {
@@ -36,3 +62,13 @@ const checkAC = (age: number) => {
         payload: age
     } as const
 }
+const checkUpAC = (age: number, filter: sortFilter) => {
+    return {
+        type: 'check-up',
+        payload: {
+            age,
+            filter
+        }
+    } as const
+}
+
